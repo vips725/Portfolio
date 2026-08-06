31b69835a314283a9954250dc460b8e7c1b8ddfc
vips725 <vipsviper2@gmail.com>
2026-08-07 04:13:11 +0530

    testing api for chatbot

--- api/chat.ts
+++ api/chat.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { buildSystemPrompt } from '../src/lib/chatContext'
+import { buildSystemPrompt } from '../src/lib/chatContext.ts'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
--- src/lib/chatContext.ts
+++ src/lib/chatContext.ts
@@ -1,4 +1,4 @@
-import { projectsData, education, experience, tools, services, aboutMe } from '@/constants'
+import { projectsData, education, experience, tools, services, aboutMe } from "../constants"
 
 export function buildSystemPrompt(): string {
   const projectsText = projectsData
